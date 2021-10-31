a3dbdd160dfb8f2626e8f95ae7b457cd003f527a
bthall73 <bthall@live.unc.edu>
2021-10-31 19:54:22 -0400

    update a single user

--- server.js
+++ server.js
@@ -43,9 +43,15 @@ app.get("/app/user/:id", (req, res) => {
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-
+app.patch("/app/update/user/:id", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?").get(req.params.id);
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(200).json({"message": info.changes + " record updated: ID " + info.lastInsertRowid + " (200)"});
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
+});
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
